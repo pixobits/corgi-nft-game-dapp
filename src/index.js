61013e2f55948bf9f361a9f80c6e25486404cb5d
Genius Mac <Genius Mac>
2021-08-10 18:49:37 +0700

    add browser router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import "./i18n";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App />
-  , document.getElementById('root'));
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+, document.getElementById('root'));
 
 serviceWorker.unregister();
